856a935167acc5cb68150f5c7c387b44bbbae3a3
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-06-15 12:22:16 -0500

    initial commit with hidden api key for mapbox-app

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="map"></div>
   </div>
 </template>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  height: 500px;
+  width: 100%;
+}
+</style>
+<script>
+/* global mapboxgl */
+export default {
+  data: function () {
+    return {};
+  },
+  methods: {},
+  mounted() {
+    mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    console.log(map);
+  },
+};
+</script>
