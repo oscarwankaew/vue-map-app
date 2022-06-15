2a502a04c6122eb2aeb1df8f54961d98626ce439
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-06-15 13:42:56 -0500

    adds a loop to display multiple markers and popups

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -17,7 +17,12 @@ body {
 /* global mapboxgl */
 export default {
   data: function () {
-    return {};
+    return {
+      places: [
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+        { lat: -33.8675, lng: 151.207, description: "The main city!" },
+      ],
+    };
   },
   methods: {},
   mounted() {
@@ -25,8 +30,13 @@ export default {
     const map = new mapboxgl.Map({
       container: "map", // container ID
       style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: [this.places[0].lng, this.places[0].lat], // starting position [lng, lat]
+      zoom: 8, // starting zoom
+    });
+
+    this.places.forEach((place) => {
+      const popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      new mapboxgl.Marker().setLngLat([place.lng, place.lat]).setPopup(popup).addTo(map);
     });
     console.log(map);
   },
